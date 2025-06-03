f73936e3d658d9b990f9e2baa133b4a708cd481c
Ajay Godara <158254090+AjayGodara2417@users.noreply.github.com>
2025-06-03 16:51:29 +0530

    Update route.ts

--- src/app/(api)/api/notes/route.ts
+++ src/app/(api)/api/notes/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 import { db } from "@/lib/db";
-import { getAuth } from "@clerk/nextjs/server";
+// import { getAuth } from "@clerk/nextjs/server";
 
 export async function POST(req: NextRequest) {
   // Ensure the user is authenticated
